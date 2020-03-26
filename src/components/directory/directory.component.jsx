8c1dcc2ce6134af7b5873c564895eaa00c91aaf7
Tom Wilson <tom.wilson@liquidint.com>
2020-03-26 12:40:13 -0400

    added shop page with preview panels

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,48 +1,16 @@
 import React from 'react';
 
 import './directory.styles.scss';
+import DIRECTORY_DATA from './directory.data.js';
 
 import MenuItem from '../menu-item/menu-item.component';
 
 class Directory extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          linkUrl: 'hats',
-          id: 1
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          linkUrl: 'jackets',
-          id: 2
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          linkUrl: 'sneakers',
-          id: 3
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          linkUrl: 'womens',
-          id: 4
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          linkUrl: 'mens',
-          id: 5
-        }
-      ]
+      sections: DIRECTORY_DATA
     };
   }
 
