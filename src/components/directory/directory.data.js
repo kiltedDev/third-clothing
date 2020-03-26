c453a510178462de32884b88eaeb1a1c168e7591
Tom Wilson <tom.wilson@liquidint.com>
2020-03-26 12:41:30 -0400

    commiting rest of shop

---
+++ src/components/directory/directory.data.js
@@ -0,0 +1,36 @@
+const DIRECTORY_DATA = [
+  {
+    title: 'hats',
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    linkUrl: 'hats',
+    id: 1
+  },
+  {
+    title: 'jackets',
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    linkUrl: 'jackets',
+    id: 2
+  },
+  {
+    title: 'sneakers',
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    linkUrl: 'sneakers',
+    id: 3
+  },
+  {
+    title: 'womens',
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    size: 'large',
+    linkUrl: 'womens',
+    id: 4
+  },
+  {
+    title: 'mens',
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    size: 'large',
+    linkUrl: 'mens',
+    id: 5
+  }
+]
+
+export default DIRECTORY_DATA;
