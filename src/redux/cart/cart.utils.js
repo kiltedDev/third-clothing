e8f7d505c593669ddd0fd879766f4d97c4bbf408
Tom Wilson <tom.wilson@liquidint.com>
2020-03-29 12:39:04 -0400

    built selectors for cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -5,12 +5,12 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   if (existingCartItem) {
     return cartItems.map( cartItem =>
-      cartItem.id == cartItemToAdd.id
+      cartItem.id === cartItemToAdd.id
       ? { ...cartItem, quantity: cartItem.quantity +1 }
       : cartItem
     )
   } else {
     return[ ...cartItems, {...cartItemToAdd, quantity: 1 }]
   }
-  
+
 }
