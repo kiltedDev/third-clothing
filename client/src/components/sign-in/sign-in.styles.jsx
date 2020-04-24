18a17afa1835ce1b9b7bd38797806105292aa777
Tom Wilson <tom.wilson@liquidint.com>
2020-04-24 13:28:59 -0400

    style updates for sign in and sign up

--- client/src/components/sign-in/sign-in.styles.jsx
+++ client/src/components/sign-in/sign-in.styles.jsx
@@ -2,8 +2,12 @@ import styled from 'styled-components';
 
 export const SignInContainer = styled.div`
   width: 380px;
+  max-width: 40vw;
   display: flex;
   flex-direction: column;
+  button {
+    max-width: 18vw;
+  }
   @media screen and (max-width: 800px) {
     width: 90vw;
     button {
--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
@@ -4,7 +4,7 @@ export const SignInAndSignUpContainer = styled.main`
   max-width: 850px;
   display: flex;
   justify-content: space-between;
-  margin: 0 auto;
+  margin: 90px auto;
   @media screen and (max-width: 800px) {
     display:grid;
     max-width: 90vw;
